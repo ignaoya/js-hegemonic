cc2badd7b2cd63a1212626c00b8ea58b929cde36
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-07-08 12:30:17 +0200

    Created CRUD api for nation table

--- routes/api.js
+++ routes/api.js
@@ -82,4 +82,82 @@ router.delete('/users/:email', (req, res) => {
 	res.json({ "message": "Success!" });
 });
 
+
+router.get('/nations', (req, res) => {
+	const sql = "select * from nation";
+	let params = [];
+	db.all(sql, params, (err, rows) => {
+		if (err) {
+			res.status(400).json({"error": err.message});
+			return;
+		}
+		res.json({
+			"message": "success",
+			"data": rows
+		})
+	});
+});
+
+router.post('/nations', (req, res) => {
+	let sql = "SELECT * FROM nation WHERE user_id = ?";
+	db.all(sql, [req.body.user_id], (err, rows) => {
+		if (err) {
+			console.log("Err is true");
+			res.status(400).send({"error": err.message});
+			return;
+		} else if (rows.length > 0) {
+			console.log(rows);
+			res.send({ "message": "User already has an active nation!" });
+			return;
+		} else {
+			sql = "INSERT INTO nation (user_id, name, population, president_id, monetary_reserves, oil_reserves) VALUES (?,?,?,NULL,?,?)";
+			const params = [req.body.user_id, req.body.name, 100, 1000, 100];
+			db.run(sql, params);
+			res.send({ "message": "success" });
+		}
+	});
+});
+
+router.get('/nations/:id', (req, res) => {
+	let sql = "SELECT * FROM nation WHERE id = ?";
+	db.all(sql, [req.params.id], (err, rows) => {
+		if (err) {
+			res.status(400).send({"error": err.message});
+			return;
+		} else if (rows.length > 0) {
+			res.json({ "nation": rows });
+			return;
+		} else {
+			res.json({ "message": "Nation not found!" });
+			return;
+		}
+	});
+});
+
+router.put('/nations/:id', (req, res) => {
+	let sql = "SELECT * FROM nation WHERE id = ?";
+	const params = [req.body.name, req.params.id];
+	db.all(sql, [req.params.id], (err, rows) => {
+		if (err) {
+			res.status(400).send({"error": err.message});
+			return;
+		} else if (rows.length > 0) {
+			sql = "UPDATE nation SET name = ? WHERE id = ?";
+			db.run(sql, params);
+			res.json({ "message": "Success!" });
+			return;
+		} else {
+			res.json({ "message": "Nation not found!" });
+			return;
+		}
+	});
+});
+
+router.delete('/nations/:id', (req, res) => {
+	let sql = "DELETE FROM nation WHERE id = ?";
+	const params = [req.params.id];
+	db.run(sql, params);
+	res.json({ "message": "Success!" });
+});
+
 module.exports = router;
